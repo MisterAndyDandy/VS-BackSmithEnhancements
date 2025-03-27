1bff4f68fc0af0bbb59d76b6fd7c521621491a40
jacopouggeri <jacopo.uggeri@gmail.com>
2025-03-27 16:16:37 +0000

    Fixed quenching and smoother interaction (particles and sound decrease with temperature), quenching evaporates water

--- BlackSmithEnhancements/src/Behavior/Block/BlockBehaviorQuenching.cs
+++ BlackSmithEnhancements/src/Behavior/Block/BlockBehaviorQuenching.cs
@@ -8,13 +8,13 @@ namespace BlackSmithEnhancements.Behavior.Block
 {
     public class BlockBehaviorQuenching : BlockBehavior
     {
-        private long _secondPasted = 360;
+        private long _lastPlaySizzleSoundMilliseconds;
 
         public BlockBehaviorQuenching(Vintagestory.API.Common.Block block) : base(block)
         {
         }
     
-        public static SimpleParticleProperties InitializeSteamEffect()
+        public static SimpleParticleProperties InitializeSteamEffect(float intensity = 1)
         {
             SimpleParticleProperties steam;
             steam = new SimpleParticleProperties(
@@ -22,12 +22,12 @@ namespace BlackSmithEnhancements.Behavior.Block
                 ColorUtil.ToRgba(50, 248, 248, 255), // first alpha, second red, three green, four blue
                 new Vec3d(),
                 new Vec3d(),
-                new Vec3f(0.1f, 0.1f, 0.1f),
-                new Vec3f(0.2f, 0.3f, 0.2f),
+                new Vec3f(0.1f, 0.1f, 0.1f) * intensity,
+                new Vec3f(0.2f, 0.3f, 0.2f) * intensity,
                 1f,
                 0.01f,
-                0.2f,
-                0.8f,
+                0.2f * intensity,
+                0.8f * intensity,
                 EnumParticleModel.Quad
             )
             {
@@ -102,72 +102,81 @@ namespace BlackSmithEnhancements.Behavior.Block
 
         public override bool OnBlockInteractStart(IWorldAccessor world, IPlayer byPlayer, BlockSelection blockSel, ref EnumHandling handling)
         {
-            base.OnBlockInteractStart(world, byPlayer, blockSel, ref handling);
-
             if (blockSel == null || byPlayer.Entity.RightHandItemSlot.Empty)
                 return false;
-
-            if (block.GetBlockEntity<BlockEntityLiquidContainer>(blockSel) is not { } entityLiquidContainer)
+            if (block.GetBlockEntity<BlockEntityLiquidContainer>(blockSel) is not { } beLiquidContainer)
                 return false;
-
             var heldStack = byPlayer.Entity.RightHandItemSlot.Itemstack;
             var temp = heldStack.Collectible.GetTemperature(world, heldStack);
-
-            if (!heldStack.Collectible.HasTemperature(heldStack) || !(temp > 20.1f))
-                return false;
-
-            if (!heldStack.Collectible.HasBehavior<ItemBehaviorQuenching>())
-                return false;
-
-            Quenching(world, heldStack, byPlayer, blockSel, (BlockLiquidContainerBase)entityLiquidContainer.Block, entityLiquidContainer, heldStack.Collectible.GetTemperature(world, heldStack));
-            heldStack.Collectible.HeldTpUseAnimation = "interactstatic";
-            _secondPasted = world.Calendar.ElapsedSeconds;
-            handling = EnumHandling.Handled;
+            if (temp < 20.1f) return false;
+            if (!heldStack.Collectible.HasBehavior<ItemBehaviorQuenching>()) return false;
+            handling = EnumHandling.PreventDefault;
+            if (world.ElapsedMilliseconds - _lastPlaySizzleSoundMilliseconds < 5000) return true;
+            _lastPlaySizzleSoundMilliseconds = world.ElapsedMilliseconds;
+            var volume = temp / 1500;
+            world.PlaySoundAt(new AssetLocation("sounds/sizzle"), blockSel.FullPosition.X, blockSel.FullPosition.Y, blockSel.FullPosition.Z, byPlayer, 1f, 4f, volume);
             return true;
-
         }
-    
-        public void Quenching(IWorldAccessor world, ItemStack heldStack, IPlayer byPlayer, BlockSelection blockSel, BlockLiquidContainerBase containerBase, BlockEntityLiquidContainer entityLiquidContainer, float temp)
+        
+        public override bool OnBlockInteractStep(
+            float secondsUsed,
+        IWorldAccessor world,
+            IPlayer byPlayer,
+        BlockSelection blockSel,
+        ref EnumHandling handling)
         {
-            var elapsedSeconds = world.Calendar.ElapsedSeconds - _secondPasted;
+            handling = EnumHandling.PreventDefault;
+            return false;
+        }
 
-            var contentStacks = IsContentWater(entityLiquidContainer.GetNonEmptyContentStacks());
+        public override void OnBlockInteractStop(float secondsUsed, IWorldAccessor world, IPlayer byPlayer, BlockSelection blockSel,
+            ref EnumHandling handling)
+        {
+            if (world.BlockAccessor.GetBlockEntity(blockSel.Position) is not BlockEntityLiquidContainer beLiquidContainer) return;
+            handling = EnumHandling.Handled;
+            var heldStack = byPlayer.Entity.RightHandItemSlot.Itemstack;
+            QuenchStack(world, heldStack, byPlayer, blockSel, beLiquidContainer);
+        }
+
+        public void QuenchStack(IWorldAccessor world, ItemStack heldStack, IPlayer byPlayer, BlockSelection blockSel, BlockEntityLiquidContainer beLiquidContainer)
+        {
+            var contentStacks = IsContentWater(beLiquidContainer.GetNonEmptyContentStacks());
 
             if (contentStacks == null) return;
 
+            var containerBase = (BlockLiquidContainerBase)beLiquidContainer.Block;
             if (containerBase.GetCurrentLitres(contentStacks) > containerBase.GetContentProps(blockSel.Position).ItemsPerLitre) return;
 
-            if (elapsedSeconds > 100)
-            {
-                if (world.Side == EnumAppSide.Client)
-                {
-                    world.PlaySoundAt(new AssetLocation("sounds/sizzle"), blockSel.FullPosition.X, blockSel.FullPosition.Y, blockSel.FullPosition.Z, byPlayer.Entity.World.PlayerByUid(byPlayer.Entity.PlayerUID), 1f, 4f, 0.8f);
-                    Particles(world, new Vec3d(entityLiquidContainer.Pos.X + 0.5f, entityLiquidContainer.Pos.Y + 0.25f, entityLiquidContainer.Pos.Z + 0.5f), InitializeSteamEffect(), InitializeWaterSplashEffect());
-                }
-            }
-            //containerBase.TryTakeContent(blockSel.Position, (int)Math.Ceiling(0.05f * BlockLiquidContainerBase.GetContainableProps(contentStacks).ItemsPerLitre)); will do something later
-            heldStack.Collectible.SetTemperature(world, heldStack, GameMath.Max(0, temp - Math.Max(0f, GameMath.Max(0f, world.Rand.Next(10, 100)))));
-
+            var temp = heldStack.Collectible.GetTemperature(world, heldStack);
+            
+            var newTemp = GameMath.Max(20, temp - Math.Max(0f, GameMath.Max(0f, world.Rand.Next(10, 100))));
+            heldStack.Collectible.SetTemperature(world, heldStack, newTemp);
+            
+            // Evaporate water
+            if (temp < 100f) return;
+            var tempDiff = temp - newTemp;
+            if (tempDiff <= 0) return;
+            var evaporateCount = (int)Math.Ceiling(tempDiff/200 * 0.05f * BlockLiquidContainerBase.GetContainableProps(contentStacks).ItemsPerLitre);
+            containerBase.TryTakeContent(blockSel.Position, evaporateCount);
+            var intensity = temp / 1500;
+            Particles(world, new Vec3d(beLiquidContainer.Pos.X + 0.5f, beLiquidContainer.Pos.Y + 0.25f, beLiquidContainer.Pos.Z + 0.5f), InitializeSteamEffect(intensity), InitializeWaterSplashEffect());
+            world.PlaySoundAt(new AssetLocation("sounds/pourmetal"), blockSel.FullPosition.X, blockSel.FullPosition.Y, blockSel.FullPosition.Z, byPlayer, 1.5f, 4f, 0.8f*intensity);
         }
 
         private static ItemStack IsContentWater(ItemStack[] contentStacks) {
             if (contentStacks.Length == 0)
                 return null;
-            bool isWater;
-            var itemStack = contentStacks[0] ?? contentStacks[1];
+            var itemStack = contentStacks.Length > 1 ? contentStacks[0] ?? contentStacks[1] : contentStacks[0];
             if (itemStack == null) return null;
-            if (itemStack.Collectible.IsLiquid()) {
-                // Check for waterportion instead for compatibility with BalancedThirst and Hydrate or Diedrate or similar mods
-                isWater = itemStack.Collectible.FirstCodePart().Contains("waterportion");
-                return isWater ? null : itemStack;
-            }
             if (!itemStack.Collectible.IsLiquid()) return null;
-            return null;
+
+            // Check for waterportion instead for compatibility with BalancedThirst and Hydrate or Diedrate or similar mods
+            var isWater = itemStack.Collectible.Code.ToString().Contains("waterportion");
+            return isWater ? itemStack : null;
         }
 
         private static void Particles(IWorldAccessor world, Vec3d vec3, SimpleParticleProperties steam, WaterSplashParticles waterSplash)
         {
-
             waterSplash.BasePos.Set(vec3);
             waterSplash.AddVelocity.Set(0, 0, 0);
             waterSplash.QuantityMul = 1f;
